890fc5ff7c47c4d85d017c94095e56fa8af7c18c
Sansevieria <elizabethleighmather@gmail.com>
2019-05-29 23:41:15 -0500

    add burger working

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,3 @@
-USE burgers_db;
-
 INSERT INTO burgers (burger_name, devoured)
 VALUES ("Bacon Burger", false), ("Cheddar Burger", false), ("Veggie Burger", false)
 , ("Vegan Burger", false), ("Gouda Burger", false);
--- public/assets/js/burgerApp.js
+++ public/assets/js/burgerApp.js
@@ -1,13 +1,38 @@
-$(function() {
+$(function () {
 
-    $("#new-burger").on("click", function(event) {
+    // change burger state on click
+    $(".change-state").on("click", function(event) {
+        var id = $(this).data("id");
+        var newDevour = $(this).data("newstate");
+
+        var newDevourState = { devoured: newDevour };
+
+        // PUT request
+        $.ajax("/api/burgers" + id, {
+            type: "PUT",
+            data: newDevourState
+        }).then(function() {
+            console.log("changed state to", newDevour);
+            location.reload();
+        });
+    });
+
+    $("#new-burger").on("click", function (event) {
         event.preventDefault();
 
         var newBurg = {
             burger_name: $("#add-burger").val().trim(),
-            devoured: false
-        }
+            devoured: 0
+        };
+
+        // POST request
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurg
+        }).then(function() {
+            console.log("new burger added");
+            location.reload();
+        });
     });
 
-    
 });
